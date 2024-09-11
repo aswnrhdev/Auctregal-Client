f0bf68a763f9c07760c8f49b0a387d2a6d73b9e2
Aswin Ramesh <>
2024-09-11 18:47:50 +0530

    url path changed

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://auctregal-client.vercel.app'
+    baseURL: 'https://auctregal.rudopedia.shop'
 });
 
 export default instance;
