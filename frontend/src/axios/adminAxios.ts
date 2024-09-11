c37c5ca330340930609fc05720cbaa9656747b10
Aswin Ramesh <>
2024-09-11 18:23:26 +0530

    auction page updated

--- frontend/src/axios/adminAxios.ts
+++ frontend/src/axios/adminAxios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { refreshToken } from "@/api/adminApi";
 
 const instance = axios.create({
-    baseURL: 'https://auctregal.rudopedia.shop'
+    baseURL: 'http://localhost:5000'
 });
 
 
