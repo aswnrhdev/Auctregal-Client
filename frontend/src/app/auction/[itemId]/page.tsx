65ffb8e90a46eca7f445eb5c7320cc8432eab9ff
Aswin Ramesh <>
2024-09-11 19:09:48 +0530

    Updated all code related to admin URLs

--- frontend/src/app/auction/[itemId]/page.tsx
+++ frontend/src/app/auction/[itemId]/page.tsx
@@ -80,7 +80,7 @@ const ItemDetails: React.FC = () => {
     useEffect(() => {
         const fetchItemDetails = async () => {
             try {
-                const response = await fetch(`http://localhost:5000/items/${itemId}`);
+                const response = await fetch(`https://auctregal.rudopedia.shop/items/${itemId}`);
                 if (!response.ok) {
                     throw new Error('Failed to fetch item details');
                 }
@@ -139,7 +139,7 @@ const ItemDetails: React.FC = () => {
         if (!item) return;
 
         try {
-            const response = await axios.post('http://localhost:5000/verify-winner', {
+            const response = await axios.post('https://auctregal.rudopedia.shop/verify-winner', {
                 email: userEmail,
                 token: biddingToken,
                 itemId: item._id
@@ -159,7 +159,7 @@ const ItemDetails: React.FC = () => {
         if (!item) return;
 
         try {
-            const response = await axios.post('http://localhost:5000/complete-auction-payment', {
+            const response = await axios.post('https://auctregal.rudopedia.shop/complete-auction-payment', {
                 itemId: item._id,
                 email: userData.email
             });
@@ -176,7 +176,7 @@ const ItemDetails: React.FC = () => {
         if (!item) return;
 
         try {
-            const response = await axios.post('http://localhost:5000/confirm-auction-payment', {
+            const response = await axios.post('https://auctregal.rudopedia.shop/confirm-auction-payment', {
                 paymentIntentId,
                 itemId: item._id,
                 email: userData.email
@@ -233,7 +233,7 @@ const ItemDetails: React.FC = () => {
         if (!item) return;
 
         try {
-            const response = await axios.post('http://localhost:5000/generate-bidding-token', {
+            const response = await axios.post('https://auctregal.rudopedia.shop/generate-bidding-token', {
                 email,
                 itemId: item._id
             });
