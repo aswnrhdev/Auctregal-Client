10074aead152cb3f51b76e82c9bf96485fa2604f
Aswin Ramesh <>
2024-09-11 18:52:52 +0530

    url path changed

--- frontend/src/axios/axios.ts
+++ frontend/src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'https://auctregal.rudopedia.shop'
+    baseURL: process.env.NEXT_PUBLIC_BASE_URL, // Use the environment variable here
 });
 
 export default instance;
