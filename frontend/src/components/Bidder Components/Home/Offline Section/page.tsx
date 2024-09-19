693ce7057ed2c5776560813d0bf71eabcb698176
Aswin Ramesh <>
2024-09-19 16:40:36 +0530

    modified home side

--- frontend/src/components/Bidder Components/Home/Offline Section/page.tsx
+++ frontend/src/components/Bidder Components/Home/Offline Section/page.tsx
@@ -11,10 +11,10 @@ export const OfflineReg = () => {
   };
 
   return (
-    <div className="relative w-full bg-[#3C3633]">
-      <div className="relative w-full h-[630px]">
+    <div className="relative w-full bg-[#361500]">
+      <div className="relative w-full h-[400px] sm:h-[500px] md:h-[630px]">
         <Image
-          src="/empty-auditorium-awaiting-big-performance-ahead-generated-by-ai.jpg"
+          src="/female-business-executive-giving-speech.jpg"
           alt="Home Banner"
           fill
           style={{ objectFit: 'cover', opacity: 0.5 }}
@@ -23,16 +23,17 @@ export const OfflineReg = () => {
       </div>
       <div className="absolute inset-0 flex flex-col items-center justify-center z-10 gap-4 px-5 md:px-10">
         <div className="text-center">
-          <h1 className="text-white font-thin tracking-tight text-4xl md:text-5xl lg:text-7xl cursor-pointer">
+          <h1 className="text-white font-thin tracking-tight text-3xl sm:text-4xl md:text-5xl lg:text-7xl cursor-pointer">
             Mega Auction
           </h1>
-          <p className="text-white font-thin mt-1">
-            Join us for the Auctregal Mega Auction on September 9, 2024, at 2:00 PM, hosted at the prestigious Septimum Hall. <br />
-            Register now to secure your spot and participate in this exciting event.
+          <p className="text-white font-thin mt-2 text-sm sm:text-base md:text-lg lg:text-xl">
+            Join us for the Auctregal Mega Auction on September 9, 2024, at 2:00 PM, <br />
+            hosted at the prestigious Septimum Hall. Register now to secure your spot <br />
+            and participate in this exciting event.
           </p>
         </div>
         <button 
-          className="bg-[#747264] text-white font-thin px-6 py-2 cursor-pointer"
+          className="bg-[#603601] text-white font-thin px-4 py-2 sm:px-6 sm:py-3 md:px-8 md:py-4 cursor-pointer"
           onClick={handleOfflineRoute}
         >
           Register now
