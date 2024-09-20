b12eae6b3a40bef29662236c47135c2c79110085
Aswin Ramesh <>
2024-09-20 10:23:58 +0530

    s3 config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 // /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-      domains: ['trippotravel.s3.ap-south-1.amazonaws.com'], // Add the S3 domain here
+      domains: ['trippotravel.s3.eu-north-1.amazonaws.com'], // Add the S3 domain here
     },
   };
   
