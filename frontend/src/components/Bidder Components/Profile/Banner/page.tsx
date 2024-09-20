c78a033a2bb7e2dd93db3db62b7b5ca7c1399f5b
Aswin Ramesh <>
2024-09-20 09:25:23 +0530

    made changes in profile page

--- frontend/src/components/Bidder Components/Profile/Banner/page.tsx
+++ frontend/src/components/Bidder Components/Profile/Banner/page.tsx
@@ -1,28 +1,56 @@
+// 'use client'
+
+// const Banner = () => {
+//     return (
+//         <>
+//             <>
+//                 <div className="bg-[#2C3639] flex justify-center items-center flex-col text-[#DCD7C9] pt-10 pb-10">
+//                     <h1 className="text-7xl font-thin">Profile</h1>
+
+//                     <div className="text-center pt-5 pb-5">
+//                         <div className="text-white font-thin bg-[#3F4E4F] pl-28 pr-28 pt-5 pb-5 w-[1000px]">
+
+
+//                             <div className="pt-5 pb-5 bg-[#DCD7C9] text-[#3F4E4F] mt-5 font-normal italic border-[#A27B5C] border-l-8">
+//                                 <p >Check your bids or update your profile anytime <br />Cheers, The Auctregal Team!</p>
+                               
+//                             </div>
+
+//                         </div>
+//                     </div>
+
+//                 </div>
+//             </>
+//         </>
+//     )
+// }
+
+// export default Banner
+
+
 'use client'
 
 const Banner = () => {
     return (
         <>
-            <>
-                <div className="bg-[#2C3639] flex justify-center items-center flex-col text-[#DCD7C9] pt-10 pb-10">
-                    <h1 className="text-7xl font-thin">Profile</h1>
+            <div className="bg-[#603601] flex justify-center items-center flex-col text-[#DCD7C9] pt-10 pb-10">
+                <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-7xl font-thin text-center">Profile</h1>
 
-                    <div className="text-center pt-5 pb-5">
-                        <div className="text-white font-thin bg-[#3F4E4F] pl-28 pr-28 pt-5 pb-5 w-[1000px]">
+                <div className="text-center pt-5 pb-5 px-4 sm:px-8 w-full">
+                    <div className="text-white font-thin bg-[#361500] px-4 sm:px-6 md:px-12 lg:px-28 py-5 w-full max-w-[1000px] mx-auto">
 
-
-                            <div className="pt-5 pb-5 bg-[#DCD7C9] text-[#3F4E4F] mt-5 font-normal italic border-[#A27B5C] border-l-8">
-                                <p >Check your bids or update your profile anytime <br />Cheers, The Auctregal Team!</p>
-                               
-                            </div>
-
+                        <div className="pt-5 pb-5 bg-[#DCD7C9] text-[#3F4E4F] mt-5 font-normal italic border-[#A27B5C] border-l-8 mx-4 sm:mx-8">
+                            <p className="text-sm sm:text-base">
+                                Check your bids or update your profile anytime <br />
+                                Cheers, The Auctregal Team!
+                            </p>
                         </div>
+                        
                     </div>
-
                 </div>
-            </>
+            </div>
         </>
     )
 }
 
-export default Banner
+export default Banner;
--- frontend/src/components/Bidder Components/Profile/User Section/page.tsx
+++ frontend/src/components/Bidder Components/Profile/User Section/page.tsx
@@ -9,15 +9,27 @@
 // import { CiEdit } from 'react-icons/ci';
 // import { FcCancel } from 'react-icons/fc';
 // import { FaCopy } from 'react-icons/fa';
+// import Swal from 'sweetalert2';
+
+// interface Bid {
+//     itemTitle: string;
+//     category: string;
+//     bidStatus: string;
+//     bidResult: string;
+//     transactionStatus: string;
+//     bidAmount: string;
+//     hasBiddingToken: boolean;
+// }
 
 // const UserInformation = () => {
 
-//     const user = useSelector((state: RootState) => state.user);  // Call useSelector here
+//     const user = useSelector((state: RootState) => state.user);
 
 //     const [image, setImage] = useState<string | null>(null);
-//     const [walletBalance, setWalletBalance] = useState<number | null>(null);
+//     const [walletBalance, setWalletBalance] = useState<string>('');
 //     const [isEditing, setIsEditing] = useState(false);
 //     const [isCancelEnabled, setIsCancelEnabled] = useState(false);
+//     const [biddingHistory, setBiddingHistory] = useState<Bid[]>([]);
 
 //     const dispatch = useDispatch();
 //     const { id, name, email, auctCode } = user;
@@ -29,13 +41,14 @@
 //                     const response = await axios.get(`https://auctregal.rudopedia.shop/user?email=${email}`);
 //                     const userData = response.data;
 //                     setImage(userData.image || null);
-//                     setWalletBalance(userData.walletBalance || 0);
+//                     setWalletBalance(userData.walletBalance || '0');
+//                     setBiddingHistory(userData.biddingHistory || []);
 //                     dispatch(setUserData({
 //                         id: userData._id,
 //                         name: userData.name,
 //                         email: userData.email,
 //                         role: userData.role,
-//                         token: userData.token, // Assuming the token is still valid
+//                         token: userData.token,
 //                         auctCode: userData.auctCode
 //                     }));
 //                 } catch (error) {
@@ -47,8 +60,9 @@
 //         fetchUserData();
 //     }, [email, dispatch]);
 
+
 //     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//         dispatch(setUserData({ ...user, name: e.target.value }));  // Update name using the user object
+//         dispatch(setUserData({ ...user, name: e.target.value }));
 //     };
 
 //     const handleImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -65,7 +79,6 @@
 //                     }
 //                 });
 //                 setImage(response.data.fileUrl);
-//                 // Note: We're not updating the Redux store here as the image is not part of the userSlice
 //             } catch (error) {
 //                 console.error('Error uploading image:', error);
 //             }
@@ -74,7 +87,20 @@
 
 //     const handleCopy = () => {
 //         navigator.clipboard.writeText(auctCode || '');
-//         alert('Code copied to clipboard!');
+
+//         Swal.fire({
+//             toast: true,
+//             position: 'top',
+//             text: 'AuctCode copied to your clipboard!',
+//             showConfirmButton: false,
+//             timer: 2000,
+//             background: 'black',
+//             color: 'white',
+//             customClass: {
+//                 popup: 'swal-toast',
+//                 title: 'swal-toast-title'
+//             }
+//         });
 //     };
 
 //     const handleEditClick = () => {
@@ -90,7 +116,7 @@
 //             const response = await axios.put('https://auctregal.rudopedia.shop/user', { name, image, email });
 //             dispatch(setUserData({
 //                 ...response.data,
-//                 token: response.data.token || '', // Ensure token is included
+//                 token: response.data.token || '',
 //             }));
 //         } catch (error) {
 //             console.error('Error saving user data:', error);
@@ -103,8 +129,8 @@
 //     };
 
 //     return (
-//         <div className='flex flex-col bg-[#2C3639] text-[#A27B5C] p-10'>
-//             <div className="pb-10 bg-[#3F4E4F] text-[#DCD7C9] mt-5 font-normal border-[#DCD7C9] border-l-8 pt-5">
+//         <div className='flex flex-col bg-[#603601] text-[#A27B5C] p-10'>
+//             <div className="pb-10 bg-[#361500] text-[#DCD7C9] mt-5 font-normal border-[#CC9544] border-l-8 pt-5">
 //                 <div className='text-center mb-5 mt-5'>
 //                     <h1 className='font-thin text-5xl tracking-tighter'>Personal Information</h1>
 //                     <p className='font-thin'>Bidding fosters competition, and a bidder&apos;s value is highlighted by their strategic choices...</p>
@@ -189,17 +215,43 @@
 //                     <p className='font-thin'>The wallet provides details of your recent transactions, including the amount and history.</p>
 
 //                     <div>
-//                         <div className="pt-5 pb-5 bg-[#DCD7C9] text-[#3F4E4F] mt-5 font-normal border-[#A27B5C] border-l-8 w-1/4">
+//                         <div className="pt-5 pb-5 bg-[#DCD7C9] text-[#3F4E4F] mt-5 font-normal border-[#CC9544] border-l-8 w-1/4">
 //                             <div className='flex items-center gap-10 ml-5'>
 //                                 <p>Wallet Balance</p>
-//                                 <p>₹{walletBalance?.toLocaleString()}</p>
+//                                 <p>{walletBalance?.toLocaleString()}</p>
 //                             </div>
 //                         </div>
 //                     </div>
 //                 </div>
 
-//                 <div className='pl-10'>
-//                     <h1 className='font-thin text-3xl tracking-tighter'>Bidding History</h1>
+//                 <div className='pl-10 pb-10'>
+//                     <h1 className='font-thin text-3xl tracking-tighter mb-5'>Bidding History</h1>
+//                     <div className="overflow-x-auto pl-10 pr-20">
+//                         <table className="min-w-full bg-[#361500] text-[#DCD7C9]">
+//                             <thead>
+//                                 <tr className="border-b border-[#A27B5C]">
+//                                     <th className="py-2 px-4 text-left font-thin">Item Name</th>
+//                                     <th className="py-2 px-4 text-left font-thin">Category</th>
+//                                     <th className="py-2 px-4 text-left font-thin">Bid Status</th>
+//                                     <th className="py-2 px-4 text-left font-thin">Bid Result</th>
+//                                     <th className="py-2 px-4 text-left font-thin">Transaction Status</th>
+//                                     <th className="py-2 px-4 text-left font-thin">Bid Amount</th>
+//                                 </tr>
+//                             </thead>
+//                             <tbody>
+//                                 {biddingHistory.map((bid, index) => (
+//                                     <tr key={index} className="border-b border-[#A27B5C] font-thin">
+//                                         <td className="py-2 px-4">{bid.itemTitle}</td>
+//                                         <td className="py-2 px-4">{bid.category}</td>
+//                                         <td className="py-2 px-4">{bid.bidStatus}</td>
+//                                         <td className="py-2 px-4">{bid.bidResult}</td>
+//                                         <td className="py-2 px-4">{bid.transactionStatus}</td>
+//                                         <td className="py-2 px-4">{bid.bidAmount}</td>
+//                                     </tr>
+//                                 ))}
+//                             </tbody>
+//                         </table>
+//                     </div>
 //                 </div>
 //             </div>
 //         </div>
@@ -207,6 +259,8 @@
 // };
 
 // export default UserInformation;
+
+
 
 
 'use client'
@@ -233,7 +287,6 @@ interface Bid {
 }
 
 const UserInformation = () => {
-
     const user = useSelector((state: RootState) => state.user);
 
     const [image, setImage] = useState<string | null>(null);
@@ -249,7 +302,7 @@ const UserInformation = () => {
         const fetchUserData = async () => {
             if (email) {
                 try {
-                    const response = await axios.get(`https://auctregal.rudopedia.shop/user?email=${email}`);
+                    const response = await axios.get(`http://localhost:5000/user?email=${email}`);
                     const userData = response.data;
                     setImage(userData.image || null);
                     setWalletBalance(userData.walletBalance || '0');
@@ -271,7 +324,6 @@ const UserInformation = () => {
         fetchUserData();
     }, [email, dispatch]);
 
-
     const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(setUserData({ ...user, name: e.target.value }));
     };
@@ -284,7 +336,7 @@ const UserInformation = () => {
             formData.append('email', email);
 
             try {
-                const response = await axios.post('https://auctregal.rudopedia.shop/upload', formData, {
+                const response = await axios.post('http://localhost:5000/upload', formData, {
                     headers: {
                         'Content-Type': 'multipart/form-data'
                     }
@@ -324,7 +376,7 @@ const UserInformation = () => {
         setIsCancelEnabled(false);
 
         try {
-            const response = await axios.put('https://auctregal.rudopedia.shop/user', { name, image, email });
+            const response = await axios.put('http://localhost:5000/user', { name, image, email });
             dispatch(setUserData({
                 ...response.data,
                 token: response.data.token || '',
@@ -340,17 +392,17 @@ const UserInformation = () => {
     };
 
     return (
-        <div className='flex flex-col bg-[#603601] text-[#A27B5C] p-10'>
-            <div className="pb-10 bg-[#361500] text-[#DCD7C9] mt-5 font-normal border-[#CC9544] border-l-8 pt-5">
+        <div className='flex flex-col bg-[#603601] text-[#A27B5C] p-6 md:p-10'>
+            <div className="pb-10 bg-[#361500] text-[#DCD7C9] mt-5 font-normal border-[#CC9544] border-l-8 pt-5 px-4 sm:px-6 md:px-8">
                 <div className='text-center mb-5 mt-5'>
-                    <h1 className='font-thin text-5xl tracking-tighter'>Personal Information</h1>
-                    <p className='font-thin'>Bidding fosters competition, and a bidder&apos;s value is highlighted by their strategic choices...</p>
+                    <h1 className='font-thin text-3xl sm:text-4xl md:text-5xl tracking-tighter'>Personal Information</h1>
+                    <p className='font-thin text-sm sm:text-base'>Bidding fosters competition, and a bidder&apos;s value is highlighted by their strategic choices...</p>
                 </div>
 
                 <div className='text-center'>
-                    <h1 className='font-thin'>Your Auct Code is given below</h1>
+                    <h1 className='font-thin text-lg sm:text-xl md:text-2xl'>Your Auct Code is given below</h1>
                     <div className='flex justify-center items-center mt-2'>
-                        <p className='font-normal text-[#A27B5C] mr-2'>{auctCode}</p>
+                        <p className='font-normal text-[#A27B5C] mr-2 text-sm sm:text-base'>{auctCode}</p>
                         <button onClick={handleCopy} className='text-[#A27B5C]'>
                             <FaCopy size={15} />
                         </button>
@@ -372,7 +424,7 @@ const UserInformation = () => {
                                     {image ? (
                                         <Image src={image} alt='Profile' width={96} height={96} objectFit="cover" />
                                     ) : (
-                                        <span className='text-sm'>Upload Image</span>
+                                        <span className='text-xs sm:text-sm'>Upload Image</span>
                                     )}
                                 </div>
                             </label>
@@ -385,7 +437,7 @@ const UserInformation = () => {
                                 type='text'
                                 id='name'
                                 value={name || ''}
-                                onChange={handleNameChange}  // Use handleNameChange callback
+                                onChange={handleNameChange}
                                 className="w-full bg-transparent border-b border-[#DCD7C9] py-2 placeholder-[#DCD7C9] focus:outline-none focus:border-[#DCD7C9]"
                                 placeholder='Name'
                                 readOnly={!isEditing}
@@ -406,13 +458,13 @@ const UserInformation = () => {
 
                     <div className='flex items-center justify-center gap-5'>
                         <button
-                            className='bg-[#DCD7C9] text-[#2C3639] px-8 py-2 rounded-md transform transition-transform duration-300 hover:scale-105'
+                            className='bg-[#DCD7C9] text-[#2C3639] px-6 sm:px-8 py-2 rounded-md transform transition-transform duration-300 hover:scale-105'
                             onClick={isEditing ? handleSaveClick : handleEditClick}
                         >
                             {isEditing ? <IoIosSave /> : <CiEdit />}
                         </button>
                         <button
-                            className={`bg-red-950 text-white px-8 py-2 rounded-md transform transition-transform duration-300 hover:scale-105 ${!isCancelEnabled ? 'opacity-50 cursor-not-allowed' : ''}`}
+                            className={`bg-red-950 text-white px-6 sm:px-8 py-2 rounded-md transform transition-transform duration-300 hover:scale-105 ${!isCancelEnabled ? 'opacity-50 cursor-not-allowed' : ''}`}
                             onClick={handleCancelClick}
                             disabled={!isCancelEnabled}
                         >
@@ -421,13 +473,13 @@ const UserInformation = () => {
                     </div>
                 </div>
 
-                <div className='p-10'>
-                    <h1 className='font-thin text-3xl tracking-tighter'>Wallet Information</h1>
-                    <p className='font-thin'>The wallet provides details of your recent transactions, including the amount and history.</p>
+                <div className='p-6 sm:p-8 text-center'>
+                    <h1 className='font-thin text-2xl sm:text-3xl tracking-tighter mb-5'>Wallet Information</h1>
+                    <p className='font-thin text-sm sm:text-base'>The wallet provides details of your recent transactions, including the amount and history.</p>
 
                     <div>
-                        <div className="pt-5 pb-5 bg-[#DCD7C9] text-[#3F4E4F] mt-5 font-normal border-[#CC9544] border-l-8 w-1/4">
-                            <div className='flex items-center gap-10 ml-5'>
+                        <div className="pt-5 pb-5 bg-[#DCD7C9] text-[#3F4E4F] mt-5 font-normal border-[#CC9544] border-l-8 w-full max-w-md mx-auto">
+                            <div className='flex items-center gap-4 ml-5'>
                                 <p>Wallet Balance</p>
                                 <p>{walletBalance?.toLocaleString()}</p>
                             </div>
@@ -435,29 +487,29 @@ const UserInformation = () => {
                     </div>
                 </div>
 
-                <div className='pl-10 pb-10'>
-                    <h1 className='font-thin text-3xl tracking-tighter mb-5'>Bidding History</h1>
-                    <div className="overflow-x-auto pl-10 pr-20">
+                <div className='pl-6 sm:pl-8 pb-10'>
+                    <h1 className='font-thin text-2xl sm:text-3xl tracking-tighter mb-5'>Bidding History</h1>
+                    <div className="overflow-x-auto">
                         <table className="min-w-full bg-[#361500] text-[#DCD7C9]">
                             <thead>
                                 <tr className="border-b border-[#A27B5C]">
-                                    <th className="py-2 px-4 text-left font-thin">Item Name</th>
-                                    <th className="py-2 px-4 text-left font-thin">Category</th>
-                                    <th className="py-2 px-4 text-left font-thin">Bid Status</th>
-                                    <th className="py-2 px-4 text-left font-thin">Bid Result</th>
-                                    <th className="py-2 px-4 text-left font-thin">Transaction Status</th>
-                                    <th className="py-2 px-4 text-left font-thin">Bid Amount</th>
+                                    <th className="py-2 px-4 text-left font-thin text-sm sm:text-base">Item Name</th>
+                                    <th className="py-2 px-4 text-left font-thin text-sm sm:text-base">Category</th>
+                                    <th className="py-2 px-4 text-left font-thin text-sm sm:text-base">Bid Status</th>
+                                    <th className="py-2 px-4 text-left font-thin text-sm sm:text-base">Bid Result</th>
+                                    <th className="py-2 px-4 text-left font-thin text-sm sm:text-base">Transaction Status</th>
+                                    <th className="py-2 px-4 text-left font-thin text-sm sm:text-base">Bid Amount</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {biddingHistory.map((bid, index) => (
                                     <tr key={index} className="border-b border-[#A27B5C] font-thin">
-                                        <td className="py-2 px-4">{bid.itemTitle}</td>
-                                        <td className="py-2 px-4">{bid.category}</td>
-                                        <td className="py-2 px-4">{bid.bidStatus}</td>
-                                        <td className="py-2 px-4">{bid.bidResult}</td>
-                                        <td className="py-2 px-4">{bid.transactionStatus}</td>
-                                        <td className="py-2 px-4">{bid.bidAmount}</td>
+                                        <td className="py-2 px-4 text-sm sm:text-base">{bid.itemTitle}</td>
+                                        <td className="py-2 px-4 text-sm sm:text-base">{bid.category}</td>
+                                        <td className="py-2 px-4 text-sm sm:text-base">{bid.bidStatus}</td>
+                                        <td className="py-2 px-4 text-sm sm:text-base">{bid.bidResult}</td>
+                                        <td className="py-2 px-4 text-sm sm:text-base">{bid.transactionStatus}</td>
+                                        <td className="py-2 px-4 text-sm sm:text-base">{bid.bidAmount}</td>
                                     </tr>
                                 ))}
                             </tbody>
